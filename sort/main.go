ae7d7c5257d54acebdd544838ad794e707a47fc3
刘宇1314 <10868657+liu-yu-1314@user.noreply.gitee.com>
2022-11-08 09:39:14 +0800

    Add files via upload

--- sort/main.go
+++ sort/main.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	arr := [...]int{10, 9, 11, 23, 13, 67, 99, 59}
 	for i := 0; i < len(arr)-1; i++ {
-		for j := i + 1; j < len(arr); j++ {
-			if arr[i] > arr[j] {
-				temp := arr[i]
-				arr[i] = arr[j]
-				arr[j] = temp
+		for j := 0; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				temp := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
 			}
 		}
 	}
